667c4c2110e4a378db0189c9bbc09dd59bb320b4
Carlos Gomez <caragomezher@unal.edu.co>
2021-12-06 21:01:19 -0500

    Cambios rutas

--- src/App.vue
+++ src/App.vue
@@ -32,57 +32,50 @@
 <script>
 export default {
   name: "App",
-  data: function () {
-    return {
-      is_auth: false,
-    };
+  computed: {
+    is_auth: {
+      get: function () {
+        return this.$route.meta.requiresAuth;
+      },
+      set: function () {},
+    },
   },
-  components: {},
   methods: {
-    verifyAuth: function () {
-      this.is_auth = localStorage.getItem("isAuth") || false;
-
-      if (this.is_auth == false) this.$router.push({ name: "logIn" });
-      else this.$router.push({ name: "home" });
-    },
-    loadHome: function () {
-      this.$router.push({ name: "home" });
-    },
     loadLogIn: function () {
       this.$router.push({ name: "logIn" });
-    },
-    loadTransaction: function () {
-      this.$router.push({ name: "transaction" });
     },
     loadSignUp: function () {
       this.$router.push({ name: "signUp" });
     },
-    loadAccount: function () {
-      this.$router.push({ name: "account" });
-    },
-    logOut: function () {
-      localStorage.clear();
-      alert("Sesión Cerrada");
-      this.verifyAuth();
-    },
     completedLogIn: function (data) {
-      localStorage.setItem("isAuth", true);
       localStorage.setItem("username", data.username);
       localStorage.setItem("token_access", data.token_access);
       localStorage.setItem("token_refresh", data.token_refresh);
       alert("Autenticación Exitosa");
-      this.verifyAuth();
+      this.loadHome();
     },
     completedSignUp: function (data) {
       alert("Registro Exitoso");
       this.completedLogIn(data);
     },
-  },
-  created: function () {
-    this.verifyAuth();
+    loadHome: function () {
+      this.$router.push({ name: "home" });
+    },
+    loadAccount: function () {
+      this.$router.push({ name: "account" });
+    },
+    loadTransaction: function () {
+      this.$router.push({ name: "transaction" });
+    },
+    logOut: function () {
+      localStorage.clear();
+      alert("Sesión Cerrada");
+      this.loadLogIn();
+    },
   },
 };
 </script>
+
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Varela+Round&display=swap");
--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,84 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import LogIn from "../components/LogIn.vue";
-import SignUp from "../components/SignUp.vue";
-import Home from "../components/Home.vue";
-import Account from "../components/Account.vue";
-import Transaction from "../components/Transaction.vue";
+import gql from "graphql-tag";
+import { createRouter, createWebHistory } from "vue-router";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import LogIn from '../components/LogIn.vue'
+import SignUp from '../components/SignUp.vue'
+import Home from '../components/Home.vue'
+import Account from '../components/Account.vue'
+import Transaction from '../components/Transaction.vue'
 
-const routes = [
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/user/login",
-    name: "logIn",
-    component: LogIn,
-  },
-  {
-    path: "/user/signUp",
-    name: "signUp",
-    component: SignUp,
-  },
-  {
-    path: "/user/home",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/user/account",
-    name: "account",
-    component: Account,
-  },
-  {
-    path: "/user/transaction",
-    name: "transaction",
-    component: Transaction,
-  },
+const routes = [{
+  path: '/user/logIn',
+  name: "logIn",
+  component: LogIn,
+  meta: { requiresAuth: false }
+},
+{
+  path: '/user/signUp',
+  name: "signUp",
+  component: SignUp,
+  meta: { requiresAuth: false }
+},
+{
+  path: '/user/home',
+  name: "home",
+  component: Home,
+  meta: { requiresAuth: true }
+},
+{
+  path: '/user/account',
+  name: "account",
+  component: Account,
+  meta: { requiresAuth: true }
+},
+{
+  path: '/user/transaction',
+  name: "transaction",
+  component: Transaction,
+  meta: { requiresAuth: true }
+}
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
+const apolloClient = new ApolloClient({
+  link: createHttpLink({ uri: 'https://api-gw-mision.herokuapp.com/' }),
+  cache: new InMemoryCache()
+})
+
+async function isAuth() {
+  if (localStorage.getItem("token_access") === null ||
+    localStorage.getItem("token_refresh") === null) {
+    return false;
+  }
+  try {
+    var result = await apolloClient.mutate({
+      mutation: gql`
+        mutation ($refresh: String!) {
+          refreshToken(refresh: $refresh) {
+            access
+          }
+        }
+        `,
+      variables: {
+        refresh: localStorage.getItem("token_refresh"),
+      },
+    })
+    localStorage.setItem("token_access", result.data.refreshToken.access);
+    return true;
+  } catch {
+    localStorage.clear();
+    alert("Su sesión expiró, por favor vuelva a iniciar sesión");
+    return false;
+  }
+}
+router.beforeEach(async (to, from) => {
+  var is_auth = await isAuth();
+  if (is_auth == to.meta.requiresAuth) return true
+  if (is_auth) return { name: "home" };
+  return { name: "logIn" };
+})
 export default router;
